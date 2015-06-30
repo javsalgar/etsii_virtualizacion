2658edd367e5d0b5fe9c3ffcccfba66f670034b0
Javier J. Salmeron Garcia <javsalgar@gmail.com>
2015-06-30 18:49:15 +0200

    Added frames

--- include/etsii_test.h
+++ include/etsii_test.h
@@ -1,6 +1,8 @@
 #ifndef ETSII_TEST_H
 #define ETSII_TEST_H
 
+#include "roi.h"
+
 namespace etsii {
 
     const struct video_analyzer::ROI test_1_frame_roi(403,260, 40, 15);
@@ -14,14 +16,17 @@ namespace etsii {
     const int test_2_characters = 23499;
     const struct video_analyzer::ROI test_2_roi(7, 289, 161, 41);
 
+    const struct video_analyzer::ROI test_3_frame_roi(94, 101, 1135, 55);
     const int test_3_begin = 44215;
     const int test_3_end = 48127;
     const struct video_analyzer::ROI test_3_roi(7, 242, 85, 34);
 
+    const struct video_analyzer::ROI test_4_frame_roi(145, 179, 69, 16);
     const int test_4_frame = 48227;
     const int test_4_clicks = 1000;
     const struct video_analyzer::ROI test_4_roi(2, 242, 84, 16);
 
+    const struct video_analyzer::ROI test_5_frame_roi(202, 273, 130, 19);
     const int test_5_begin = 55227;
     const int test_5_end = 66244;
     const struct video_analyzer::ROI test_5_roi(500, 548, 368, 48);
--- src/main.cpp
+++ src/main.cpp
@@ -13,18 +13,20 @@ int main()
 {
 
     std::shared_ptr<cv::VideoCapture> ptr2(new cv::VideoCapture("/home/javsalgar/salidausuario10 06_22_2015_01_09_02.avi"));
-    std::shared_ptr<cv::VideoCapture> ptr(new cv::VideoCapture("/home/javsalgar/salidausuario1 06_15_2015_05_02_13.avi"));
+    std::shared_ptr<cv::VideoCapture> ptr(new cv::VideoCapture("/home/javsalgar/salidausuario10 06_22_2015_01_09_02.avi"));
+//    std::shared_ptr<cv::VideoCapture> ptr(new cv::VideoCapture("/home/javsalgar/salidausuario1 06_15_2015_05_38_02.avi"));
     video_analyzer::FrameExtractor f_ext(ptr);
     video_analyzer::FrameExtractor f_ext2(ptr2);
 
-//    cv::Mat im1 = f_ext2.extractFrame(24990, etsii::test_2_frame_roi);
+//    cv::Mat im1 = f_ext2.extractFrame(44080, etsii::test_5_frame_roi);
 
-    cv::Mat im1 = cv::imread("/home/javsalgar/lossless2.png");
+    cv::Mat im1 = cv::imread("/home/javsalgar/lossless5.png");
 
-    cv::namedWindow("test2");
-    cv::imshow("test2", im1);
-    cv::waitKey(0);
-    cv::destroyWindow("test2");
+//    cv::namedWindow("test2");
+//    cv::imshow("test2", im1);
+//    cv::waitKey(0);
+//    cv::destroyWindow("test2");
+
 
 
 //    std::vector<int> compression_params;
@@ -32,14 +34,16 @@ int main()
 //    compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
 //    compression_params.push_back(0);
 
-//    cv::imwrite("/home/javsalgar/lossless2.png", im1, compression_params);
+//    cv::imwrite("/home/javsalgar/lossless5.png", im1, compression_params);
 
-    int res = video_analyzer::seekFrame(ptr, im1, etsii::test_2_frame_roi);
+    int res = video_analyzer::seekFrame(ptr, im1, etsii::test_5_frame_roi);
 
     if (res != -1) {
         cv::namedWindow("test");
         cv::imshow("test", f_ext.extractFrame(res));
         cv::waitKey(0);
+    } else {
+        std::cout << "Not found!" << std::endl;
     }
 
     return 0;
